c6659f1336a1bf5a67710cb7dbf03bf7bc766212
wangchao <wcd126yx@126.com>
2019-08-16 11:11:14 +0800

    增加、

--- mock.js
+++ mock.js
@@ -4,6 +4,13 @@ var questions = [
             { content: " ;<ux-mth>\\frac{ 1}{110 }</ux-mth> ;" },
             { content: " ;<ux-mth>\\frac{1 }{n(n+1) }</ux-mth> ;" }
         ],
-        content: "<span>1、如图，在直角梯形ABCD中，AB∥CD，AD⊥DC，AB=BC，且AE⊥BC．&nbsp;（1）求证：AD=AE；&nbsp;（2）若AD=8，DC=4，求AB的长．</span><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>"
+        // content: "若集合<ux-mth>A=\\left\\{\\left(x,y\\right)|\\left(x-1\\right)^2+\\left(y-2\\right)^2\\le\\frac{4}{5}\\right\\}</ux-mth>与<ux-mth>B=\\left\\{\\left(x,y\\right)|\\left|x-1\\right|+2\\left|y-2\\right|\\le a\\right\\}</ux-mth>是直角坐标系<ux-mth>xOy</ux-mth>内的点集，则<ux-mth>A\\subseteq B</ux-mth>的充要条件为<ux-blank sequence=1></ux-blank>．"
+        // content: "记集合<ux-mth>T=\\left\\{0,1,2,3,4,5,6\\right\\}</ux-mth>，<ux-mth>M=\\left\\{\\frac{a_1}{7}+\\frac{a_2}{7^2}+\\frac{a_3}{7^3}+\\frac{a_4}{7^4}|a_i\\in T,i=1,2,3,4\\right\\}</ux-mth>，将<ux-mth>M</ux-mth>中的元素按从大到小的顺序排列，则第2009个数是<ux-blank sequence=1></ux-blank>．"
+        // content: "下列各数中：&nbsp;<ux-mth>0.3</ux-mth>&nbsp;、&nbsp;<ux-mth>\\sqrt[]{4}</ux-mth>&nbsp;、&nbsp;<ux-mth>\\pi-3</ux-mth>&nbsp;、&nbsp;<ux-mth>\\sqrt[]{5}</ux-mth>&nbsp;、&nbsp;<ux-mth>3.14</ux-mth>&nbsp;、&nbsp;<ux-mth>1.51511511\\ldots</ux-mth>&nbsp;，有理数有&nbsp;<ux-blank sequence=1></ux-blank>&nbsp;个，无理数有&nbsp;<ux-blank sequence=2></ux-blank>&nbsp;个．"
+        // content: "集合<ux-mth>A=\\left\\{x|x=\\frac{\\left|a\\right|}{a}+\\frac{b}{\\left|b\\right|},a,b\\in R\\right\\}</ux-mth> ，用列举法表示集合<ux-mth>A=</ux-mth><ux-blank sequence=1></ux-blank>．"
+        // content: "如图，点<ux-mth>C</ux-mth>在线段<ux-mth>AB</ux-mth>上，<ux-mth>AC=8</ux-mth> cm，<ux-mth>CB=6</ux-mth> cm，点<ux-mth>M</ux-mth>、<ux-mth>N</ux-mth>分别是<ux-mth>AC</ux-mth>、<ux-mth>BC</ux-mth>的中点．（1）求线段<ux-mth>MN</ux-mth>的长；2）若<ux-mth>C</ux-mth>为线段<ux-mth>AB</ux-mth>上任一点，满足<ux-mth>AC+CB=a</ux-mth> cm，其它条件不变，你能猜想<ux-mth>MN</ux-mth>的长度吗？并说明理由；3）若<ux-mth>C</ux-mth>在线段<ux-mth>AB</ux-mth>的延长线上，且满足<ux-mth>AC-BC=b</ux-mth>cm，<ux-mth>M</ux-mth>、<ux-mth>N</ux-mth>分别为<ux-mth>AC</ux-mth>、<ux-mth>BC</ux-mth>的中点，你能猜想<ux-mth>MN</ux-mth>的长度吗？请画出图形，写出你的结论，并说明理由；（4）你能用一句简洁的话，描述你发现的结论吗？<img src='/dd?fn=1418017921613.png'>"
+        content: "设集合<ux-mth>U=\\left\\{\\left(x,y\\right)|x\\in R,y\\in R\\right\\},A=\\left\\{\\left(x,y\\right)|2x-y+m>0\\right\\},B=\\left\\{\\left(x,y\\right)|x+y-n\\le0\\right\\}</ux-mth>，那么点<ux-mth>P\\left(2,3\\right)\\in A\\bigcap\\left(C_UB\\right)</ux-mth>的充要条件是（&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ）"
+
+
     }
 ]
